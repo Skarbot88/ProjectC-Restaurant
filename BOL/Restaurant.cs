8e579c7ad80b461df16eb4004ddb526794043252
Sundhar Karunanidi <sundhar.karunanidi@gmail.com>
2021-05-12 16:54:55 +0100

    Overall project created. Business Object Layer, Classes Defined for the rest of the layers to reference.

---
+++ BOL/Restaurant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BOL
+{
+    public class Restaurant
+    {
+        [Key]
+        public int RestaurantId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Contact { get; set; }
+    }
+}
