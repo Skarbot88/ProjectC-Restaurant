b91d36ca7605d231b0778bab65ba040e7846d424
Sundhar Karunanidi <sundhar.karunanidi@gmail.com>
2021-05-12 17:57:14 +0100

    Business logic layer integration. Business Logic classes for Cart, Restaurant and Items entities.

---
+++ BLL/CartBs.cs
@@ -0,0 +1,50 @@
+﻿using BOL;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL
+{
+    public interface ICartBs
+    {
+        IEnumerable<Cart> GetAll();
+        Cart GetById(int id);
+
+        bool Insert(Cart obj);
+        bool Update(Cart obj);
+        bool Delete(int id);
+    }
+    public class CartBs: ICartBs
+    {
+        private ICartDb objDb;
+        public CartBs(ICartDb _objDb)
+        {
+            objDb = _objDb;
+        }
+        public bool Delete(int id)
+        {
+            return objDb.Delete(id);
+        }
+
+        public IEnumerable<Cart> GetAll()
+        {
+            return objDb.GetAll();
+        }
+
+        public Cart GetById(int id)
+        {
+            return objDb.GetById(id);
+        }
+
+        public bool Insert(Cart obj)
+        {
+            return objDb.Insert(obj);
+        }
+
+        public bool Update(Cart obj)
+        {
+            return objDb.Update(obj);
+        }
+    }
+}
---
+++ BLL/ItemsBs.cs
@@ -0,0 +1,50 @@
+﻿using BOL;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL
+{
+    public interface IItemsBs
+    {
+        IEnumerable<Items> GetAll();
+        Items GetById(int id);
+
+        bool Insert(Items obj);
+        bool Update(Items obj);
+        bool Delete(int id);
+    }
+    public  class ItemsBs: IItemsBs
+    {
+        private IItemsDb objDb;
+        public ItemsBs(IItemsDb _objDb)
+        {
+            objDb = _objDb;
+        }
+        public bool Delete(int id)
+        {
+            return objDb.Delete(id);
+        }
+
+        public IEnumerable<Items> GetAll()
+        {
+            return objDb.GetAll();
+        }
+
+        public Items GetById(int id)
+        {
+            return objDb.GetById(id);
+        }
+
+        public bool Insert(Items obj)
+        {
+            return objDb.Insert(obj);
+        }
+
+        public bool Update(Items obj)
+        {
+            return objDb.Update(obj);
+        }
+    }
+}
